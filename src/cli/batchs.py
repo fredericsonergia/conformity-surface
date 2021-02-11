6982d8fe35ae44eddf373987ae216a398493fc94
Jerkow <jerome.kowalski@student-cs.fr>
2021-02-11 12:38:25 +0100

    cli tests

--- src/cli/batchs.py
+++ src/cli/batchs.py
@@ -47,6 +47,7 @@ class BatchComputer():
             sc.get_surfaces()
             sc.get_confidence(model)
             self.result += str(sc) + ",\n"
+        self.result = self.result[:-2]
         self.result += ']'
 
     def save(self, file_name):
@@ -56,25 +57,25 @@ class BatchComputer():
         file = open(file_name, "w")
         file.write(self.result)
 
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--config", required=True,
+                    help="path to the configuration file")
+    ap.add_argument("-m", '--maj', required=False,
+                    help="update the building data")
+    args = vars(ap.parse_args())
+    MAJ = False if "maj" in args.keys() is None else args["maj"]
+    config = configparser.ConfigParser()
+    config.read(args["config"])
+    Image, Batch, Confidence, Data = config['IMAGE'], config['BATCH'], config['CONFIDENCE'], config['DATA']
+    w, h, r, R = int(Image["width in px"]), int(Image["height in px"]), float(
+        Image["ratio in px/m"]), float(Image["Radius in m"])
+    input_file, output_file = Batch["input"], Batch["output"]
+    data_path, static_path = Data["data_path"], Data["static_path"]
+    model = pickle.load(open(Confidence["model_path"], 'rb'))
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--config", required=True,
-                help="path to the configuration file")
-ap.add_argument("-m", '--maj', required=False,
-                help="update the building data")
-args = vars(ap.parse_args())
-MAJ = False if "maj" in args.keys() is None else args["maj"]
-config = configparser.ConfigParser()
-config.read(args["config"])
-Image, Batch, Confidence, Data = config['IMAGE'], config['BATCH'], config['CONFIDENCE'], config['DATA']
-w, h, r, R = int(Image["width in px"]), int(Image["height in px"]), float(
-    Image["ratio in px/m"]), float(Image["Radius in m"])
-input_file, output_file = Batch["input"], Batch["output"]
-data_path, static_path = Data["data_path"], Data["static_path"]
-model = pickle.load(open(Confidence["path"], 'rb'))
-
-computer = BatchComputer(static_path=static_path, data_path=data_path, file_name=input_file, MAJ=args["maj"])
-computer.load_data()
-computer.extract_coordinates()
-computer.get_all(w, h, r, R, model)
-computer.save(output_file)
+    computer = BatchComputer(static_path=static_path, data_path=data_path, file_name=input_file, MAJ=args["maj"])
+    computer.load_data()
+    computer.extract_coordinates()
+    computer.get_all(w, h, r, R, model)
+    computer.save(output_file)
